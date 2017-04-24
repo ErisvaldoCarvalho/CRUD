21dfe34627953f50f1897374b42ecc27b4d03029
CarvalhoForte <contato@erisvaldocarvalho.com.br>
2017-04-24 11:12:00 -0300

    Estrutura da Coluna e da Tabela pronta.

--- Solucao/Modelo/Coluna.cs
+++ Solucao/Modelo/Coluna.cs
@@ -7,5 +7,25 @@ namespace Modelo
 {
     public class Coluna
     {
+        private string nomeColuna;
+        private string parametroSQL;
+        private bool chavePrimaria;
+        private bool aceitaNulo;
+        private int quantidadeDeCaracteres;
+        private object valor;
+        private object valorPadrao;
+
+        public object Valor { get {return valor; } set {valor = value; } }
+        public object ValorPadrao { get { return valorPadrao; } set { valorPadrao = value; } }
+        public bool ChavePrimaria { get { return chavePrimaria; }set {chavePrimaria = value; AceitaNulo = false; } }
+        public string ParametroSQL { get { return parametroSQL; } set {parametroSQL = value; } }
+        public int QuantidadeDeCaracteres { get {return quantidadeDeCaracteres; } set {quantidadeDeCaracteres = value; } }
+        public bool AceitaNulo { get { return aceitaNulo; } set { aceitaNulo = value; } }
+        public string NomeColuna { get { return nomeColuna; } set { nomeColuna = value; ParametroSQL = "@" + value.ToString(); if (value == "@Codigo") ChavePrimaria = true; } }
+
+        public Coluna()
+        {
+            AceitaNulo = true;
+        }
     }
 }
--- Solucao/Modelo/Tabela.cs
+++ Solucao/Modelo/Tabela.cs
@@ -8,11 +8,32 @@ namespace Modelo
     public class Tabela
     {
         private string nomeTabela;
+        private Coluna codigo;
+        private Coluna sequencia;
+        private Coluna codigoEntidade;
+        private Coluna dataCadastro;
 
-        public string NomeTabela { get { return nomeTabela; } set {nomeTabela = value; } }
+        public string NomeTabela { get { return nomeTabela; } set { nomeTabela = value; } }
+        public int Codigo { get { return Convert.ToInt32(codigo.Valor); } set { codigo.Valor = Convert.ToInt32(value); } }
+        public int Sequencia { get { return Convert.ToInt32(sequencia.Valor); } set { sequencia.Valor = Convert.ToInt32(value); } }
+        public int CodigoEntidade { get { return Convert.ToInt32(codigoEntidade.Valor); } set { codigoEntidade.Valor = Convert.ToInt32(value); } }
+        public DateTime DataCadastro { get { return Convert.ToDateTime(dataCadastro.Valor); } set { dataCadastro.Valor = Convert.ToDateTime(value); } }
 
         public Tabela()
         {
+            codigo = new Modelo.Coluna();
+            sequencia = new Coluna();
+            codigoEntidade = new Coluna();
+            dataCadastro = new Coluna();
+            
+            codigo.NomeColuna = "Codigo";
+            sequencia.NomeColuna = "Sequencia";
+            sequencia.AceitaNulo = false;
+            codigoEntidade.NomeColuna = "CodigoEntidade";
+            codigoEntidade.AceitaNulo = false;
+            dataCadastro.NomeColuna = "DataCadastro";
+            dataCadastro.ValorPadrao = "GETDATE()";
+            
 
         }
     }
